6b3d29ec884300b083d3c590279b05754707100c
nmanikumar5 <nmanikumar5@gmail.com>
2017-12-21 17:38:48 +0530

    serverjstext changed

--- server.js
+++ server.js
@@ -77,12 +77,11 @@ router.route('/sampleText')
  
  router.route('/getusers')
 .get(function(req, res) {
-	var user = new User();
-   user.find(function(err, user) {
+   User.find(function(err, users) {
   if(err) {
   res.send(err);
   }
-  res.json(user);
+  res.json(users);
   });
  });
 
